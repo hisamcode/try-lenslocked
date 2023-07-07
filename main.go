16c07eb552e607e2d6ad6d9a8be0a7d802060445
hisamcode <hisamcode@gmail.com>
2023-07-07 08:38:05 +0700

    add logger middleware

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,6 +26,7 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)

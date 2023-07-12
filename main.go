c1d5dd6d222ab7c02ce46a307d29ad6d513f3778
hisamcode <hisamcode@gmail.com>
2023-07-12 10:10:13 +0700

    implement must function on top level

--- main.go
+++ main.go
@@ -15,31 +15,21 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	// parse the template
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/", controllers.StaticHandler(tpl))
+	r.Get("/", controllers.StaticHandler(
+		views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))))
 
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/contact", controllers.StaticHandler(tpl))
+	r.Get("/contact", controllers.StaticHandler(
+		views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
 
-	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/faq", controllers.StaticHandler(tpl))
+	r.Get("/faq", controllers.StaticHandler(
+		views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
 	log.Println("Starting server on 3000")
-	err = http.ListenAndServe("127.0.0.1:3000", r)
+	err := http.ListenAndServe("127.0.0.1:3000", r)
 	if err != nil {
 		log.Fatal("error listen server :", err)
 	}

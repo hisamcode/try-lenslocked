7e1c2f4c8752d9a1f29cee982e933b65c09ffda4
hisamcode <hisamcode@gmail.com>
2023-07-13 10:49:15 +0700

    Made the FAQ page use dynamically provided question and answer

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/hisamcode/lenslocked/views"
@@ -9,5 +10,32 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version ?",
+			Answer:   "Yes! we offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have support staff answering emails 24/7, though response time may be a bit slower on weekends.",
+		},
+		{
+			Question: "How do i contact support?",
+			Answer:   `Email us - <a href="mailto:hisamcode@gmail.com">hisamcode@gmail.com</a>`,
+		},
+		{
+			Question: "Do you have office located?",
+			Answer:   `Our entire team is remotely`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }

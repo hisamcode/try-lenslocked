8d208c085c403b235da0574020f8ef4a1ab3bedf
hisamcode <hisamcode@gmail.com>
2023-07-17 08:58:34 +0700

    Added the users.Create function to handle the signup form

--- controllers/users.go
+++ controllers/users.go
@@ -1,19 +1,22 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
-
-	"github.com/hisamcode/lenslocked/views"
 )
 
 type Users struct {
 	Templates struct {
-		New views.Template
+		New Template
 	}
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
-	// we need view to render
 	u.Templates.New.Execute(w, nil)
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Email : ", r.FormValue("email"))
+	fmt.Fprint(w, "Password : ", r.FormValue("password"))
 
 }

18ed6706278349f1ee94a5cf93778cd74428556f
hisamcode <hisamcode@gmail.com>
2023-07-20 16:37:54 +0700

    experiment postgre

---
+++ cmd/postgre/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+type PostgreConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgreConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
+func main() {
+	cfg := PostgreConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "admin",
+		Password: "password",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+	db, err := sql.Open("pgx", cfg.String())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("connected")
+}
